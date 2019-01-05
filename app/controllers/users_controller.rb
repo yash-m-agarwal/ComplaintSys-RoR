0eb2792682fda6b08ada2a57bf220602cd23f3e1
Mishal Shah <shahmishal1998@gmail.com>
2019-01-05 16:50:43 +0530

    user id not found fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :require_user, only: [:show]
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(session[:user_id])
     @complaints = @user.complaints
   end
 
